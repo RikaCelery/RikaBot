70919c683513233d0cd42653da89df119ce7e6f3
RikaCelery <qincai233@gmail.com>
2024-08-23 17:02:54 +0800

    update(changelog)

--- change_log.go
+++ change_log.go
@@ -10,7 +10,10 @@ var changeLog = []changeLogStruct{
 		Version: "2024-08-23",
 		ChangeLog: `新功能：
 + 在使用未知指令时尝试寻找对应插件并进行提示
++ {prefix}更新日志 [指定版本号,可省略]  显示更新日志
++ 插件/picpick: 图片收藏插件 （没写完）
+
 修复:
-+ quote: 修复渲染历史消息时少一条`,
++ 插件/quote: 修复渲染历史消息时少一条`,
 	},
 }
