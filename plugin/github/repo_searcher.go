d8fad5d077c3dd13c178ab353155de177a6acaa6
RikaCelery <qincai233@gmail.com>
2024-10-10 12:24:38 +0800

    feat(github): 自动监听并渲染发送的GitHub链接

--- plugin/github/repo_searcher.go
+++ plugin/github/repo_searcher.go
@@ -2,9 +2,15 @@
 package github
 
 import (
+	"github.com/FloatTech/ZeroBot-Plugin/utils"
+	"github.com/playwright-community/playwright-go"
+	log "github.com/sirupsen/logrus"
+	"github.com/wdvxdr1123/ZeroBot/extension"
+	"github.com/wdvxdr1123/ZeroBot/extension/shell"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/FloatTech/floatbox/web"
 	ctrl "github.com/FloatTech/zbpctrl"
@@ -17,17 +23,67 @@ import (
 )
 
 func init() { // 插件主体
-	control.AutoRegister(&ctrl.Options[*zero.Ctx]{
+	e := control.AutoRegister(&ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
-		Brief:            "GitHub仓库搜索",
-		Help: "- >github [xxx]\n" +
-			"- >github -p [xxx]",
-	}).OnRegex(`^>github\s(-.{1,10}? )?(.*)$`).SetBlock(true).
+		Brief:            "GitHub相关",
+		Help: "- {prefix}github [xxx]\n" +
+			"- {prefix}github -p [xxx]",
+	})
+	e.OnRegex(`(github.com\/([^\/]+)\/([^\/]+)(?:(\/pulls|\/issues|\/discussions|\/actions(?:\/runs)?)(\/\d+)?)?[a-zA-Z0-9-_+\/#.%&=\[\]|\\]*)`).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			log.Debugf("[github] regex matched: %v", ctx.State["regex_matched"].([]string))
+			model := &extension.RegexModel{}
+			ctx.Parse(model)
+			switch expression := model.Matched[4]; expression {
+			case "/actions/runs":
+			case "/actions":
+				fallthrough
+			case "/pulls":
+				fallthrough
+			case "/issues":
+				fallthrough
+			case "/discussions":
+				fallthrough
+			default:
+				bytes, err := utils.ScreenShotElementURL(
+					model.Matched[1],
+					"turbo-frame",
+					utils.ScreenShotElementOption{Width: 850,
+						Sleep: time.Millisecond * 1000, PwOption: playwright.ElementHandleScreenshotOptions{
+							Style: playwright.String(`.gh-header-sticky,
+.gh-header-shadow,
+.gh-header-show .gh-header-actions,
+#issues-index-tip {
+    position: static !important;
+    display: none !important;
+}
+
+turbo-frame {
+    padding-top: 10px;
+    padding-bottom: 20px;
+}
+
+.discussion-timeline-actions {
+    display: none;
+}
+`),
+						}},
+				)
+				if err != nil {
+					log.Errorln(err)
+					return
+				}
+				ctx.Send(message.ImageBytes(bytes).Add("cache", 0))
+
+			}
+		})
+	e.OnCommand(`github`).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			// 发送请求
+			args := shell.Parse(ctx.State["args"].(string))
 			api, _ := url.Parse("https://api.github.com/search/repositories")
 			api.RawQuery = url.Values{
-				"q": []string{ctx.State["regex_matched"].([]string)[2]},
+				"q": []string{args[len(args)-1]},
 			}.Encode()
 			body, err := web.RequestDataWithHeaders(&http2.DefaultClient, api.String(), "GET", func(r *http.Request) error {
 				r.Header.Set("User-Agent", web.RandUA())
@@ -44,7 +100,7 @@ func init() { // 插件主体
 			}
 			repo := info.Get("items.0")
 			// 发送结果
-			switch ctx.State["regex_matched"].([]string)[1] {
+			switch args[0] {
 			case "-p ": // 图片模式
 				ctx.SendChain(
 					message.Image(
