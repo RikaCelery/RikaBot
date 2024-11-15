2e4c0231fd9d31712ef301e0223c82d9b7452298
RikaCelery <qincai233@gmail.com>
2024-11-15 13:30:09 +0800

    feat(windows): 在warn和error级别下显示调用位置

--- console/console_windows.go
+++ console/console_windows.go
@@ -2,9 +2,7 @@
 package console
 
 import (
-	"bytes"
 	"os"
-	"strings"
 	"syscall"
 	"unsafe"
 
@@ -82,63 +80,5 @@ func init() {
 	err = setConsoleTitle("ZeroBot-Plugin " + banner.Version + " " + banner.Copyright)
 	if err != nil {
 		panic(err)
-	}
-}
-
-const (
-	colorCodePanic = "\x1b[1;31m" // color.Style{color.Bold, color.Red}.String()
-	colorCodeFatal = "\x1b[1;31m" // color.Style{color.Bold, color.Red}.String()
-	colorCodeError = "\x1b[31m"   // color.Style{color.Red}.String()
-	colorCodeWarn  = "\x1b[33m"   // color.Style{color.Yellow}.String()
-	colorCodeInfo  = "\x1b[37m"   // color.Style{color.White}.String()
-	colorCodeDebug = "\x1b[32m"   // color.Style{color.Green}.String()
-	colorCodeTrace = "\x1b[36m"   // color.Style{color.Cyan}.String()
-	colorReset     = "\x1b[0m"
-)
-
-// logFormat specialize for zbp
-type logFormat struct {
-	hasColor bool
-}
-
-// Format implements logrus.Formatter
-func (f logFormat) Format(entry *logrus.Entry) ([]byte, error) {
-	buf := new(bytes.Buffer)
-
-	buf.WriteByte('[')
-	if f.hasColor {
-		buf.WriteString(getLogLevelColorCode(entry.Level))
-	}
-	buf.WriteString(strings.ToUpper(entry.Level.String()))
-	if f.hasColor {
-		buf.WriteString(colorReset)
-	}
-	buf.WriteString("] ")
-	buf.WriteString(entry.Message)
-	buf.WriteString(" \n")
-
-	return buf.Bytes(), nil
-}
-
-// getLogLevelColorCode 获取日志等级对应色彩code
-func getLogLevelColorCode(level logrus.Level) string {
-	switch level {
-	case logrus.PanicLevel:
-		return colorCodePanic
-	case logrus.FatalLevel:
-		return colorCodeFatal
-	case logrus.ErrorLevel:
-		return colorCodeError
-	case logrus.WarnLevel:
-		return colorCodeWarn
-	case logrus.InfoLevel:
-		return colorCodeInfo
-	case logrus.DebugLevel:
-		return colorCodeDebug
-	case logrus.TraceLevel:
-		return colorCodeTrace
-
-	default:
-		return colorCodeInfo
 	}
 }
